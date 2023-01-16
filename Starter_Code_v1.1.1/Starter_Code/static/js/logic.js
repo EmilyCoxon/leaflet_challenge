636293bcb4f787beccf21f52a3e07a1f0ff401ec
Emily Coxon <emilyecoxon@gmail.com>
2023-01-16 14:49:42 +0800

    almost completed challenge, just need to finish legend

--- Starter_Code_v1.1.1/Starter_Code/static/js/logic.js
+++ Starter_Code_v1.1.1/Starter_Code/static/js/logic.js
@@ -9,69 +9,8 @@ let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geo
 d3.json(url).then(function(data) {
   createFeatures(data.features);
 
-//when response recieved, send data.features object to createFeatures function
-//     function markerSize(magnitude){
-//         return magnitude*2;
-//     }
+
 });
-//magnitude, location and depth
-
-
-function createFeatures(earthquakeData) {
-
-    function onEachFeature(feature, layer){
-        layer.bindPopup(`<h2>Location: ${feature.properties.place}</h2><hr><h3>Magnitude: ${feature.properties.mag}</h3><hr>
-        <p>Depth(km): ${feature.geometry.coordinates[2]}</p><hr><p>${new Date(feature.properties.time)}</p>`);
-    }
-
-    let earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature
-    });
-
-    createMap(earthquakes);
-}
-
-// Define a markerSize() function that will give each city a different radius based on its magnitude
-let mag = (features.properties.mag);
-function markerSize(mag) {
-  return mag * 2;
-}
-
-
-
-
-// create colour profile for magnitude and depth 
-
-// //  // Loop through the cities array, and create one marker for each city object.
-//  for (let i = 0; i < feature.length; i++) {
-  
-//     // Conditionals for country gdp_pc
-//     let color = "";
-//     if (feature[i].geometry.coordinates[2] > 10) {
-//       color = "yellow";
-//     }
-//     else if (feature[i].geometry.coordinates[2]> 20) {
-//       color = "blue";
-//     }
-//     else if (feature[i].geometry.coordinates[2] > 30) {
-//       color = "green";
-//     }
-//     else {
-//       color = "violet";
-//     }
-  
-//     // Add circles to the map.
-//     L.circle(feature[i].properties.mag, {
-//       fillOpacity: 0.75,
-//       color: "white",
-//       fillColor: color,
-//       // Adjust the radius.
-//       radius: Math.sqrt(feature[i].properties.mag) * 500
-//     }).addTo(myMap);
-
-// }
-
-
 
 function createMap(earthquakes) {
 
@@ -102,6 +41,7 @@ function createMap(earthquakes) {
     layers: [street, earthquakes]
   });
 
+
   // Create a layer control.
   // Pass it our baseMaps and overlayMaps.
   // Add the layer control to the map.
@@ -111,46 +51,80 @@ function createMap(earthquakes) {
 
 }
 
-
-
-
-
-
-
-
-
-
-
 // 2. Import and visualise the data by doing the following:
-
+//magnitude, location and depth
 
 // 2.1 Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude. 
-//feature.coordinates.longitude
-//feature.coordinates.latitude
-//feature.coordinates.depth
-
 // Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by colour.
 //
 //feature.properties.mag
 //
-//feature.coordinates.depth
+//feature.coordinates.geography[2]
+function createFeatures(earthquakeData) {
 
-
+  let earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature,
+      pointToLayer: function(feature, latlng) {
+        return L.circleMarker(latlng, {
+          radius: markerSize(feature.properties.mag),
+          fillColor: myColour(feature.geometry.coordinates[2]),
+          fillOpacity: 0.9,
+          color: "#000000",
+          stroke: true,
+          weight: 0.3
+        })
+      }
+  });
 
 //Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in colour.
 
+
+function markerSize(mag) {
+  // let mag = (feature.properties.mag);
+  return mag * 5;
+}
+
 // Hint: The depth of the earth can be found as the third coordinate for each earthquake.
 
+function myColour(depth) {
+  switch(true) {
+    case depth > 90:
+      return "#660066";
+    case depth > 70:
+      return "#800080";
+    case depth > 50:
+      return "#993366";
+    case depth > 30:
+      return "#FF8080";
+    case depth > 10:
+      return "#FF99CC";
+    default:
+      return "#FFCC99";
+  }
+}
 
+  createMap(earthquakes);
+}
+// Define a markerSize() function that will give each city a different radius based on its magnitude
 
 
 
 // 2.2 Include popups that provide additional information about the earthquake when its associated marker is clicked.
 
-
-
-
+function onEachFeature(feature, layer){
+  layer.bindPopup(`<h2>Location: ${feature.properties.place}</h2><hr><h3>Magnitude: ${feature.properties.mag}</h3><hr>
+  <p>Depth (km): ${feature.geometry.coordinates[2]}</p><hr><p>Time/Date: ${new Date(feature.properties.time)}</p>`);
+}
 
 
 // 2.3 Create a legend that will provide context for your map data.
+
+
+
+
+
+
+
+
+
 
